5ccc1455c19bd8fe8fd6b61cbe43d30f0f30f5cb
Sasha Yumzya <yumzya@email.com>
2017-05-29 19:20:09 +0000

    Update project.clj

--- project/project.clj
+++ project/project.clj
@@ -3,8 +3,8 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [aleph "0.3.0-alpha2"]
                  [compojure "1.1.1"]
-                 [ring "1.1.0-beta2"]
-                 [hiccup "1.0.0-beta1"]
+                 [ring "1.1.6"]
+                 [hiccup "1.0.1"]
                  [lein-swank "1.4.4"]]
   :source-paths ["src/clj"]
   :ring {:handler core.main/-main}
