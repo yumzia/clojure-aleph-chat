db4b68f5d6a999db9b866f760deddcc8b483eb45
Sasha Yumzya <yumzya@email.com>
2017-05-29 19:20:28 +0000

    Update

--- project/project.clj
+++ project/project.clj
@@ -1,6 +1,6 @@
 (defproject project "0.01"
   :description "Aleph testing"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [aleph "0.3.0-alpha2"]
                  [compojure "1.1.1"]
                  [ring "1.1.6"]
